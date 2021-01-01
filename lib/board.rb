73ea64d19597e8f55848a91bd8f6523e8f203977
kimalibennett <aminbennett@gmail.com>
2021-01-01 14:22:03 -0600

    commit tree 42173d430fbf0fbf15fb78d05b1b4ed3b0fee776

---
+++ lib/board.rb
@@ -0,0 +1,49 @@
+require "pry"
+class Board 
+    attr_accessor :cells
+    
+    def initialize 
+        reset!
+    end 
+    
+    def reset!
+        @cells = Array.new(9," ")
+    end
+
+    def display
+        puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+        puts "-----------"
+        puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+        puts "-----------"
+        puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+      end
+
+    def position(input)
+        cells[input.to_i-1]
+    end
+   
+    
+  def full?
+    self.cells.all? {|cell| cell == "X" || cell == "O"}
+  end
+
+  def turn_count
+    self.cells.count("X") + self.cells.count("O")
+  end
+
+  
+  def taken?(position) #check board position
+    self.cells[position.to_i-1] == "X" || #board position -1 because of test using range 1-9 (user input numbers)
+    self.cells[position.to_i-1] == "O"
+  end
+
+  
+  def valid_move?(position)
+    !taken?(position) && position.to_i >0 && position.to_i <=9
+  end
+
+  def update(position, player)
+    self.cells[position.to_i-1] = player.token
+end
+
+end
